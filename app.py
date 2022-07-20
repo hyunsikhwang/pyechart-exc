100806acabd3f271989dcc169ed91f79ea2b3946
Hyunsik <gomting@naver.com>
2022-07-20 23:10:02 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from pyecharts import options as opts
+from pyecharts.charts import TreeMap
+from streamlit_echarts import st_pyecharts
+import streamlit as st
+
+
+data = [
+    {
+        "value": 40,
+        "name": "클래스 A",
+    },
+    {
+        "value": 180,
+        "name": "클래스 B",
+        "children": [
+            {
+                "value": 76,
+                "name": "B",
+                "children": [
+                    {
+                        "value": 12,
+                        "name": "b-a",
+                    },
+                    {
+                        "value": 28,
+                        "name": "b-b",
+                    },
+                    {
+                        "value": 20,
+                        "name": "b-c",
+                    },
+                    {
+                        "value": 16,
+                        "name": "b-d",
+                    }]
+            }]}
+]
+
+treemap = TreeMap()
+treemap.add("이건뭐지", data)
+st_pyecharts(treemap)
