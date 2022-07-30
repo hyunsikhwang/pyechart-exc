4fc1f91c827ffeb3a251007ccf857ad3f1b6256d
Hyunsik Hwang <gomting@naver.com>
2022-07-30 23:33:55 +0900

    multi tabs

--- app.py
+++ app.py
@@ -21,55 +21,63 @@ def get_bs(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     return bs4.BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
 
-response = get_bs(url)
 
-x_axis = []
-y_axis = []
+tab1, tab2 = st.tabs(["Fear and Greed Index", "Buffet Index"])
 
-line = Line()
+with tab1:
+    response = get_bs(url)
 
-for itm in json.loads(response.text)['fear_and_greed_historical']['data']:
-    x_axis.append(datetime.fromtimestamp(itm['x']/1000).strftime('%Y-%m-%d'))
-    y_axis.append(itm['y'])
+    x_axis = []
+    y_axis = []
 
-line = (
-    Line(init_opts=opts.InitOpts(width="100%", height="800px"))
-    .add_xaxis(x_axis)
-    .add_yaxis('Index',
-               y_axis,
-               markpoint_opts=opts.MarkPointOpts(
-                data=[opts.MarkPointItem(name="Current", type_=None, coord=[x_axis[-1], y_axis[-1]], value=f"{y_axis[-1]:.1f}")]),
-               is_smooth=True,
-               is_step=False,
-               label_opts=opts.LabelOpts(
-                    formatter=JsCode(
-                        "function(params){return params.value[1].toFixed(1);}"
-                    )
-               ),
-               markline_opts=opts.MarkLineOpts(data=[opts.MarkLineItem(type_="average")]),
+    line = Line()
+
+    for itm in json.loads(response.text)['fear_and_greed_historical']['data']:
+        x_axis.append(datetime.fromtimestamp(itm['x']/1000).strftime('%Y-%m-%d'))
+        y_axis.append(itm['y'])
+
+    line = (
+        Line(init_opts=opts.InitOpts(width="100%", height="800px"))
+        .add_xaxis(x_axis)
+        .add_yaxis('Index',
+                y_axis,
+                markpoint_opts=opts.MarkPointOpts(
+                    data=[opts.MarkPointItem(name="Current", type_=None, coord=[x_axis[-1], y_axis[-1]], value=f"{y_axis[-1]:.1f}")]),
+                is_smooth=True,
+                is_step=False,
+                label_opts=opts.LabelOpts(
+                        formatter=JsCode(
+                            "function(params){return params.value[1].toFixed(1);}"
+                        )
+                ),
+                markline_opts=opts.MarkLineOpts(data=[opts.MarkLineItem(type_="average")]),
+        )
+        .set_series_opts(markarea_opts=opts.MarkAreaOpts(
+                data=[
+                    opts.MarkAreaItem(name="EXTREME FEAR", y=(0, 25), itemstyle_opts=opts.ItemStyleOpts(color="red", opacity=0.2)),
+                    opts.MarkAreaItem(name="FEAR", y=(25, 45), itemstyle_opts=opts.ItemStyleOpts(color="orange", opacity=0.2)),
+                    opts.MarkAreaItem(name="NEUTRAL", y=(45, 55), itemstyle_opts=opts.ItemStyleOpts(color="yellow", opacity=0.2)),
+                    opts.MarkAreaItem(name="GREED", y=(55, 75), itemstyle_opts=opts.ItemStyleOpts(color="green", opacity=0.2)),
+                    opts.MarkAreaItem(name="EXTREME GREED", y=(75, 100), itemstyle_opts=opts.ItemStyleOpts(color="blue", opacity=0.2)),
+                ],
+        ))
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="CNN Fear and Greed Index"),
+            tooltip_opts=opts.TooltipOpts(
+                formatter=JsCode(
+                    "function (params) {return params.value[0] + '<br>' + params.value[1].toFixed(1);}"
+                )
+            ),
+            xaxis_opts=opts.AxisOpts(interval=0,
+                                    boundary_gap=False,)
+        )
+        .render_embed()
     )
-    .set_series_opts(markarea_opts=opts.MarkAreaOpts(
-            data=[
-                opts.MarkAreaItem(name="EXTREME FEAR", y=(0, 25), itemstyle_opts=opts.ItemStyleOpts(color="red", opacity=0.2)),
-                opts.MarkAreaItem(name="FEAR", y=(25, 45), itemstyle_opts=opts.ItemStyleOpts(color="orange", opacity=0.2)),
-                opts.MarkAreaItem(name="NEUTRAL", y=(45, 55), itemstyle_opts=opts.ItemStyleOpts(color="yellow", opacity=0.2)),
-                opts.MarkAreaItem(name="GREED", y=(55, 75), itemstyle_opts=opts.ItemStyleOpts(color="green", opacity=0.2)),
-                opts.MarkAreaItem(name="EXTREME GREED", y=(75, 100), itemstyle_opts=opts.ItemStyleOpts(color="blue", opacity=0.2)),
-            ],
-    ))
-    .set_global_opts(
-        title_opts=opts.TitleOpts(title="CNN Fear and Greed Index"),
-        tooltip_opts=opts.TooltipOpts(
-            formatter=JsCode(
-                "function (params) {return params.value[0] + '<br>' + params.value[1].toFixed(1);}"
-            )
-        ),
-        xaxis_opts=opts.AxisOpts(interval=0,
-                                 boundary_gap=False,)
-    )
-    .render_embed()
-)
-#line.render()
+    #line.render()
 
-#st_pyecharts(line)
-components.html(line, height=800)
+    #st_pyecharts(line)
+    components.html(line, height=800)
+
+
+with tab2:
+    st.subheader("Buffet Index")
