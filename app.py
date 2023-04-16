a5e5e95817d6b4d487d188b27a92a87deb612932
Hyunsik Hwang <gomting@naver.com>
2023-04-16 22:39:02 +0900

    test

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ Ratio_min = 0.58
 Ratio_high = 0.91
 Ratio_low = 0.75
 
-df_tmp = df_new[(df_new['TRD_DD'].loc['2023-01-01':'2023-04-16'])]
+df_tmp = df_new[(df_new['TRD_DD'].between('2023-01-01', '2023-04-16'))]
 
 df_new['TRD_DD'] = pd.to_datetime(df_new['TRD_DD'])
 df_new[band] = np.where((df_new['TRD_DD'].dt.month == 1) & (df_new['TRD_DD'].dt.day == 1), initRatio * df_new['AnnSum'], np.nan)
