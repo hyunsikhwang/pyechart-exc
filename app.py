2b6d2e891e425ced2a861bcc26cd14887f5a0fff
Hyunsik Hwang <gomting@naver.com>
2023-08-15 21:12:21 +0900

    get_bs

--- app.py
+++ app.py
@@ -30,6 +30,20 @@ def get_bs(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     return bs4.BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
 
+def get_bs_KRX(url, params):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {
+                'Host': 'data.krx.co.kr',
+                'Connection': 'keep-alive',
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'ko,en-US;q=0.9,en;q=0.8',
+    }
+    return bs4.BeautifulSoup(requests.get(url, headers=headers, params=params).text, "lxml")
+
 
 tab1, tab2, tab3, tab4 = st.tabs(["Fear and Greed Index", "Buffet Index(Korea)", "Dashboard", "Treemap(Korea)"])
 
@@ -389,7 +403,7 @@ def maxworkdt_command():
               'key': 'B128.bld',
               'menuId': 'MDC0201030108'}
 
-    MktData = get_bs(url, params=params)
+    MktData = get_bs_KRX(url, params=params)
 
     data = json.loads(MktData.text)
     
